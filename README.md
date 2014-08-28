dc39ff5d23a0c2651cbd2135ba6e5ae0f385e649
Andrea Giardini <andrea.giardini5@studio.unibo.it>
2014-08-28 12:37:36 +0200

    First Draft

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 Arduino-RC-Car
 ==============
+
+This repository contains the code used to build an Arduino Robot controlled by a traditional PSX joystick.
+
+Robot
+-----
+
+The robot was build using an Arduino Due with Wifi Shield (remember to short-circuit pins 3 and 7 or the shield won't be recognized by the Due) and a L298 module to control two different motors.
+I used the Magician chassis to assemble the robot and fix all the stuffs on it, you can find it very easily online.
+
+
+Controller
+----------
+
+I used a Raspberry Pi as a remote controller for the robot (but you can use whatever you want, even your laptop). Using a Joystick adapter I
+connected a PSX controller to it.
+
+Interaction
+-----------
+
+When a joystick button is pressed the computer processes the event and sends a command to the Arduino, the command is interpreted by the robot
+that acts consequentially.
---
+++ RemoteControl.c
@@ -0,0 +1,126 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <linux/joystick.h>
+
+char arduIp[15] = "192.168.1.109";
+unsigned short int serverPort = 8080;
+
+char msg;
+
+int open_joystick(char *device_name) {
+ int fd = -1;
+
+ if (device_name == NULL) {
+   return fd;
+ }
+
+ fd = open(device_name, O_RDONLY | O_NONBLOCK);
+
+ if (fd < 0) {
+   printf("Could not locate joystick device %s\n", device_name);
+   exit(1);
+ }
+
+ return fd;
+}
+
+void print_device_info(int fd) {
+  int axes=0, buttons=0;
+  char name[128];
+
+  ioctl(fd, JSIOCGAXES, &axes);
+  ioctl(fd, JSIOCGBUTTONS, &buttons);
+  ioctl(fd, JSIOCGNAME(sizeof(name)), &name);
+
+  printf("%s\n  %d Axes %d Buttons\n", name, axes, buttons);
+}
+
+void process_event(struct js_event jse) {
+  if (jse.type == 2) {
+    if (jse.number == 0) {
+      if (jse.value < 0) {
+//	printf("SINISTRA\n");
+	msg = 'S';
+      } else if (jse.value > 0) {
+//	printf("DESTRA\n");
+	msg = 'D';
+      } else {
+//        printf("STOP\n");
+	msg = 'H';
+      }
+    } else {
+      if (jse.value < 0) {
+//	printf("AVANTI\n");
+	msg = 'A';
+      } else if (jse.value > 0) {
+//	printf("INDIETRO\n");
+	msg = 'I';
+      } else {
+//        printf("STOP\n");
+        msg = 'H';
+      }
+    }
+  }
+
+  if (jse.type == 1 && jse.value > 0) {
+    printf("%d\n", jse.number);
+    //sprintf(msg, "%d\n", jse.number);
+    //strcpy(msg, jse.number);
+  }
+}
+
+int main() {
+  int fd, i=0;
+  struct js_event jse;
+
+  fd = open_joystick("/dev/input/js1");
+  print_device_info(fd);
+
+  struct sockaddr_in serverAddress;
+  int socketDescriptor;
+
+int listenfd,connfd,n;
+   struct sockaddr_in servaddr,cliaddr;
+   socklen_t clilen;
+   pid_t     childpid;
+   char mesg[1000];
+
+   listenfd=socket(AF_INET,SOCK_STREAM,0);
+
+   bzero(&servaddr,sizeof(servaddr));
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+   servaddr.sin_port=htons(serverPort);
+   bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+
+   listen(listenfd, 1024);
+
+   clilen=sizeof(cliaddr);
+   connfd = accept(listenfd,(struct sockaddr *)&cliaddr,&clilen);
+
+   printf("GOT CONNECTION\n");
+  while (1) {
+    while (read(fd, &jse, sizeof(jse)) > 0) {
+      process_event(jse);
+      if(i>20){
+        printf("Send message...");
+        printf("%c\n",msg);
+        if(sendto(connfd,&msg,1,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr)) < 0){
+//        if(sendto(socketDescriptor, msg, strlen(msg), 0, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0){
+          printf("Unable to send data to server");
+	  exit(0);
+        }
+      }
+      i++;
+
+    }
+  }
+
+  return 0;
+}
+
